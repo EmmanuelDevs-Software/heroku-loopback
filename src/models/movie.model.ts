0b48d2ab9fd7ed0afae7761318e9372c1ca7eec5
chadwick <desarrollos.chadwick@gmail.com>
2022-06-03 11:46:57 +0200

    master

---
+++ src/models/movie.model.ts
@@ -0,0 +1,44 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Movie extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'array',
+    itemType: 'string',
+  })
+  stars?: string[];
+
+  @property({
+    type: 'number',
+  })
+  year?: number;
+
+  @property({
+    type: 'number',
+  })
+  review?: number;
+
+
+  constructor(data?: Partial<Movie>) {
+    super(data);
+  }
+}
+
+export interface MovieRelations {
+  // describe navigational properties here
+}
+
+export type MovieWithRelations = Movie & MovieRelations;
